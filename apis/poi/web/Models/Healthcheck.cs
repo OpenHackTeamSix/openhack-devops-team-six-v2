e83356cad7b803df8a3cdfb84a53c38c233382eb
K4hL <69721059+K4hL@users.noreply.github.com>
2020-09-09 21:10:14 +0200

    Update Healthcheck.cs

--- apis/poi/web/Models/Healthcheck.cs
+++ apis/poi/web/Models/Healthcheck.cs
@@ -8,7 +8,7 @@ namespace poi.Models
         public Healthcheck()
         {
             Message = "POI Service Healthcheck";
-            Status = "Healthy";
+            Status = "Super Healthy!!!!!!";
         }
         [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
         public string Message {get;set;}
