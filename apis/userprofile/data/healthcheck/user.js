7594d0437270ccf42c7ffa8e4c6e1b5a5bf1aba5
K4hL <69721059+K4hL@users.noreply.github.com>
2020-09-10 11:05:31 +0200

    Update user.js

--- apis/userprofile/data/healthcheck/user.js
+++ apis/userprofile/data/healthcheck/user.js
@@ -15,7 +15,7 @@ module.exports = {
     get: {
         200: function (req, res, callback) {
             res.json({
-                message: 'healthcheck (updated in a new branch)',
+                message: 'healthcheck (updated in a new branch!)',
                 status: 'healthy'
             });
             callback;
