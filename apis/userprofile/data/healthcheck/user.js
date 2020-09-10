2ce26ca5350a27e8bdeed12fcb8257643e790508
Kirill Larionov <36848763+KirillLarionov@users.noreply.github.com>
2020-09-10 11:50:54 +0300

    Update user.js

--- apis/userprofile/data/healthcheck/user.js
+++ apis/userprofile/data/healthcheck/user.js
@@ -15,7 +15,7 @@ module.exports = {
     get: {
         200: function (req, res, callback) {
             res.json({
-                message: 'healthcheck (updated)',
+                message: 'healthcheck (updated again)',
                 status: 'healthy'
             });
             callback;
